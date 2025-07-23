054b24a2c8ed074b90ccb8ccad7020a9da4694f1
thainaramachado <dev.thainara@gmail.com>
2025-07-23 17:24:14 -0300

    feat: adicionada função para leitura de CSV

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import csv
 import time 
-import dotenv
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
+does_not_exist = os.getenv("CSV_HEADER_1")
+inbox_full = os.getenv("CSV_HEADER_2")
+refused = os.getenv("CSV_HEADER_3")
+
+# Essa parte do codigo é pra abrir o CSV e ler
+with open('csv/data.csv', 'r', newline='') as arquivo:            
+    conteudo = arquivo.read()                                   
+    print(conteudo)
+
+# for does_not_exist in conteudo:
+#     print("Funcionou")
 
