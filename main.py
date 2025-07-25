64ea922217a93bffb61909bae2d59919a8ad6c75
thainaramachado <dev.thainara@gmail.com>
2025-07-25 16:38:27 -0300

    feat: Adicionado a função que realiza a leitura, separação e cria os arquivos csv necessários

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import csv
-import time 
 import os
 from dotenv import load_dotenv
 
@@ -9,11 +8,33 @@ does_not_exist = os.getenv("CSV_HEADER_1")
 inbox_full = os.getenv("CSV_HEADER_2")
 refused = os.getenv("CSV_HEADER_3")
 
-# Essa parte do codigo é pra abrir o CSV e ler
-with open('csv/data.csv', 'r', newline='') as arquivo:            
-    conteudo = arquivo.read()                                   
-    print(conteudo)
 
-# for does_not_exist in conteudo:
-#     print("Funcionou")
+# Cria uma lista de variaveis para usar no loop
+bounces = [does_not_exist, inbox_full, refused]
+
+# Estrutura de dados com listas vazias para cada coluna
+def init_data(bounces):
+    return {bounce: [] for bounce in bounces}
+
+# Dicionário com as colunas desejadas
+slip_data = init_data(bounces)
+
+# Essa parte do codigo é pra abrir o CSV e ler
+with open('csv/data.csv', 'r', newline='', encoding='utf-8') as file:            
+    file_content = csv.DictReader(file)                                  
+    for row in file_content:
+        reason = row.get("Motivo do bounce")
+        if reason in slip_data:
+            slip_data[reason].append({
+                "Motivo do bounce": reason,
+                "Destinatário": row.get("Destinatário")
+            })
+
+# Vai criar um CSV para cada variável
+for bounce, data in slip_data.items():
+    if bounce:
+        with open(f"csv/{bounce}.csv", mode="w", encoding="utf-8", newline="") as file:
+            output_writer = csv.DictWriter(file, fieldnames=["Motivo do bounce", "Destinatário"])
+            output_writer.writeheader()
+            output_writer.writerows(data)
 
